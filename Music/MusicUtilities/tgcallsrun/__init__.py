2149b79204bb850659a74813f1f28e13a11702ce
Neyork <99612597+Neyork@users.noreply.github.com>
2022-02-21 22:33:24 +0200

    Add files via upload

--- Music/MusicUtilities/tgcallsrun/__init__.py
+++ Music/MusicUtilities/tgcallsrun/__init__.py
@@ -4,6 +4,6 @@ from Music import config
 from Music.MusicUtilities.tgcallsrun.queues import (clear, get, is_empty, put, task_done)
 from Music.MusicUtilities.tgcallsrun.downloader import download
 from Music.MusicUtilities.tgcallsrun.convert import convert
-from Music.MusicUtilities.tgcallsrun.music import queues
+from Music.MusicUtilities.tgcallsrun.music import (convert, download)
 from Music.MusicUtilities.tgcallsrun.music import smexy as ASS_ACC
 smexy = 1
