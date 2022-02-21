0865d5df767328a3f6b497b9f0b3384b2d196483
Neyork <99612597+Neyork@users.noreply.github.com>
2022-02-21 22:11:45 +0200

    Add files via upload

--- Music/MusicUtilities/tgcallsrun/__init__.py
+++ Music/MusicUtilities/tgcallsrun/__init__.py
@@ -4,7 +4,6 @@ from Music import config
 from Music.MusicUtilities.tgcallsrun.queues import (clear, get, is_empty, put, task_done)
 from Music.MusicUtilities.tgcallsrun.downloader import download
 from Music.MusicUtilities.tgcallsrun.convert import convert
-from Music.MusicInline import (audio_markup, audio_timer_markup_start,
-                          primary_markup, secondary_markup2, timer_markup)
+from Music.MusicUtilities.tgcallsrun.music import run
 from Music.MusicUtilities.tgcallsrun.music import smexy as ASS_ACC
 smexy = 1
