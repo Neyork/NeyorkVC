e318c6ed96ca37bd1acb5a9bfbe1de98038ad835
Neyork <99612597+Neyork@users.noreply.github.com>
2022-02-25 03:16:27 +0200

    Add files via upload

--- Music/MusicUtilities/helpers/decorators.py
+++ Music/MusicUtilities/helpers/decorators.py
@@ -1,4 +1,4 @@
-from typing import Callable, Dict, List
+from typing import Callable, Union, List
 
 from pyrogram import Client
 from pyrogram.types import Chat, Message
@@ -8,7 +8,7 @@ from Music import SUDOERS
 admins = {}
 
 
-admins: Dict[int, List[int]] = {}
+admins: Union[int, List[int]] = {}
 
 
 def set(chat_id: int, admins_: List[int]):
